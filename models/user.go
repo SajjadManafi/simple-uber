7d33ed7bcb246ba2d06009e27ec9aba36c08d8b6
SajjdManafi <sajadmanafi8@gmail.com>
2022-02-19 00:13:18 +0330

    Replace user-related structures

--- models/user.go
+++ models/user.go
@@ -14,3 +14,30 @@ type User struct {
 	Email    string    `json:"email"`
 	JoinedAt time.Time `json:"joined_at"`
 }
+
+// CreateUserParams used in creating user in database
+type CreateUserParams struct {
+	Username       string `json:"username"`
+	HashedPassword string `json:"hashed_password"`
+	FullName       string `json:"full_name"`
+	Gender         Gender `json:"gender"`
+	Email          string `json:"email"`
+}
+
+// ListUsersParams used in get list of users
+type ListUsersParams struct {
+	Limit  int32 `json:"limit"`
+	Offset int32 `json:"offset"`
+}
+
+// UpdateUserParams used in update user balance using user id
+type UpdateUserParams struct {
+	ID      int32 `json:"id"`
+	Balance int64 `json:"balance"`
+}
+
+// AddUserBalanceParams used in add user balance using user id
+type AddUserBalanceParams struct {
+	Amount int64 `json:"amount"`
+	ID     int32 `json:"id"`
+}
